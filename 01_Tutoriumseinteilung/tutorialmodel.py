71e8eab15e9fbafaeb91beeb1afb6b39dc0233c9
Dominik Chmiel <dominik.chmiel@r3coms.com>
2016-05-29 16:54:55 +0200

    Optimized looping

--- 01_Tutoriumseinteilung/tutorialmodel.py
+++ 01_Tutoriumseinteilung/tutorialmodel.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python3
 
 from gurobipy import *
+import itertools
 
 def solve(nStudents, nTutorials, nTeams, groups, preferences, l, u):
 
     model = Model("tutorials")
 
     # Please use and adapt (no renaming!) the following variables:
-    
     x = {}
     for s in range(nStudents):
         for t in range(nTutorials):
@@ -18,11 +17,10 @@ def solve(nStudents, nTutorials, nTeams, groups, preferences, l, u):
     for t in range(nTutorials):
         # should have value 1 if tutorial t takes place and value 0 otherwise
         y[t] = model.addVar(  name="y_" +str(t) , vtype = "b")
-        
+
     # end of given variables; you can introduce your own variables but make sure that the above variables attain values as stated in the comments
-        
+
     model.modelSense = GRB.MAXIMIZE
-    
     model.update()
 
     #constraints
@@ -30,12 +28,12 @@ def solve(nStudents, nTutorials, nTeams, groups, preferences, l, u):
     # jeder student wird zugewiesen
     for s in range(nStudents):
         model.addConstr(quicksum(x[s,t] for t in range(nTutorials)) == 1, name="Student zugewiesen_" + str(s))
-    
+
     # benoetigte tutorien werden geoeffnet
     for s in range(nStudents):
         for t in range(nTutorials):
             model.addConstr(x[s,t] <= y[t], name="Student_" + str(s) + "besucht_Tutorium_" + str(t))
-    
+
     # tutorium mingroesse
     for t in range(nTutorials):
         model.addConstr(quicksum(x[s,t] for s in range(nStudents)) >= l[t]*y[t], name= "mingroesse_Tutorium_" + str(t))
@@ -47,14 +45,14 @@ def solve(nStudents, nTutorials, nTeams, groups, preferences, l, u):
     # nur tutorien mit praeferenz groesser null
     for s in range(nStudents):
         model.addConstr(quicksum(preferences[s][t]*x[s,t] for t in range(nTutorials)) >= 1, name="valid_tutorium_fuer" + str(s))
-    
+
     # teamgleichheit
-    for t in range(nTutorials):
+    for t, team in itertools.product(range(nTutorials), range(nTeams)):
         for s1 in range(nStudents):
-            for s2 in range(nStudents):
-                    for team in range(nTeams):
-                        if s1 in groups[team] and s2 in groups[team]:
-                            model.addConstr(x[s1,t] == x[s2,t])
+            if s1 in groups[team]:
+                 for s2 in range(nStudents):
+                      if s2 in groups[team]:
+                           model.addConstr(x[s1,t] == x[s2,t])
 
 
     def printSolution():
@@ -71,5 +69,3 @@ def solve(nStudents, nTutorials, nTeams, groups, preferences, l, u):
     model.optimize()
 #    printSolution()
 
-
- 
