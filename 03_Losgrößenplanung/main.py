a6d20fbf6572b9689ed6776ab15ad776c63dc24e
Dominik Chmiel <dominik.chmiel@r3coms.com>
2016-06-14 12:21:11 +0200

    Updated documentation

--- 03_Losgrößenplanung/main.py
+++ 03_Losgrößenplanung/main.py
@@ -5,4 +5,6 @@ from runlotsizing import LotSolver
 
 #logging.basicConfig(level='DEBUG', format="%(asctime)s [%(levelname)-5.5s] %(funcName)s : %(message)s")
 
-solver = LotSolver("lotData3.txt")
+solver = LotSolver("lotData1.txt")
+solver2 = LotSolver("lotData2.txt")
+solver3 = LotSolver("lotData3.txt")
--- 03_Losgrößenplanung/runlotsizing.py
+++ 03_Losgrößenplanung/runlotsizing.py
@@ -83,6 +83,7 @@ class LotSolver(object):
         if not filePath.endswith('.txt'):
             logging.error("Not a valid input file.")
             return False
+        self.store = {}
         with open(filePath) as f:
             content = f.read()
             return self.process_txt(content)
@@ -95,33 +96,8 @@ class LotSolver(object):
 
         model = Model("LotSolver")
 
-        """
-            Problem:
-                Timehorizon: Number of periods
-                l: Anfangslagerbestand
-                ab t=1:
-                    In jeder periode Muss bedarf von Kunden gedeckt werden (d[i])
-                    Ueberschuessiges in lager, kosten von h[i] / time
-                    Pro periode K[i] stunden / maschine
-                    Pro produkt [i] a[i] stunden
-                    1x umruesten / maschine / periode, kosten s[i][j], st[i][j] std (von prod i => j)
-
-            variables:
-                x_[prod]_[period]: produziert / periode
-                l_[prod]_[period]: lager von periode => periode + 1
-                switch_[p1]_[p2]_[period]: does maschine switch from p1 to p2 in period
-
-
-            constraints:
-                l_[prod]_[period+1] = l_[prod]_[period] + x_[prod]_[period] - d_prod_period
-                sum(switch[x][p2][t]) == sum(switch[p2][x][t+1])
-                l_[prod]_[period] + x_[prod]_[period] > d_prod_period
-                x_prod_period * a_prod + switch_p1_p2_period * st_p1_p2
-                quicksum(switch[x][y][t]) == 1
-        """
-
-        tHor = int(self.store["Timehorizon"])
-        nPr = int(self.store["nProducts"])
+        tHor = self.store["Timehorizon"]
+        nPr = self.store["nProducts"]
 
         timeRange = range(1, tHor + 1)
         prodRange = range(1, nPr + 1)
@@ -143,10 +119,8 @@ class LotSolver(object):
         for t in range(0, tHor+1):
             for p1 in prodRange:
                 for p2 in prodRange:
-                    if p1 != p2:
-                        objective = float(self.store["s"][p1-1][p2-1])
-                    else:
-                        objective = 0
+                    # Switching to the same product does not cost anything - even if the file may say otherwise
+                    objective = float(self.store["s"][p1-1][p2-1]) if p1 != p2 else 0
                     s[p1, p2, t] = model.addVar(name="s_%d_%d_%d" % (p1, p2, t), vtype="b", obj=objective)
 
         model.modelSense = GRB.MINIMIZE
@@ -168,40 +142,34 @@ class LotSolver(object):
                 model.addConstr(quicksum(s[key] for key in s if key[2] == t and (key[1] == p or key[0] == p)) * x[p,t] == x[p,t], name="single_switch_" + str(t))
 
         for t in timeRange:
+            # Allow only a single switch each period
             logging.debug('Single switch constraint for ' + str([s[key].varName for key in s if key[2] == t]))
             model.addConstr(quicksum(s[key] for key in s if key[2] == t) == 1, name="single_switch_" + str(t))
 
+            # Only allow connected switches between t-1 and t
+            for p in prodRange:
+                logging.debug('valid_switch for ' + str([s[key].varName for key in s if key[2] == (t-1) and key[1] == p]) + " and " + str([s[key].varName for key in s if key[2] == t and key[0] == p]))
+                model.addConstr(quicksum(s[key] for key in s if key[2] == (t-1) and key[1] == p) == quicksum(s[key] for key in s if key[2] == t and key[0] == p), 
+                    name="valid_switch_%d_%d" % (p, t))
 
-            if 1 or t != 1:
-                for p in prodRange:
-                    logging.debug('valid_switch for ' + str([s[key].varName for key in s if key[2] == (t-1) and key[1] == p]) + " and " + str([s[key].varName for key in s if key[2] == t and key[0] == p]))
-                    model.addConstr(quicksum(s[key] for key in s if key[2] == (t-1) and key[1] == p) == quicksum(s[key] for key in s if key[2] == t and key[0] == p), 
-                        name="valid_switch_%d_%d" % (p, t))
 
-        # Bedarf periode 0
-        for p in range(1, len(self.store["l"])+1):
-            logging.debug('initial %s + %d >= %d', x[p, 1].varName, self.store["l"][p-1], self.store["d"][p-1][0])
-            model.addConstr(x[p, 1] + self.store["l"][p-1] >= self.store["d"][p-1][0])
-
-        # Bedarf ab periode 1
-        for t in range(2, tHor+1):
+        # Demands for each period need to be met
+        for t in range(1, tHor+1):
             for p in prodRange:
                 logging.debug('%s + %s >= %d', x[p, t].varName, l[p, t-1].varName, self.store["d"][p-1][t-1])
                 model.addConstr(x[p, t] + l[p, t-1] >= self.store["d"][p-1][t-1])
 
-        # Stundenzahl
+        # Machine can't be occupied for more then K hours / period
         for t in timeRange:
-
             logging.debug("sum {} + sum {} <= {}".format([x[key].varName + "*" + str(self.store["a"][key[0]-1]) for key in x if key[1] == t],
                 [s[key].varName + "*" + str(self.store["st"][key[0]-1][key[1]-1]) for key in s if key[2] == t], self.store["K"][t-1]))
-
             model.addConstr(quicksum(x[key]*self.store["a"][key[0]-1] for key in x if key[1] == t) + quicksum(s[key]*self.store["st"][key[0]-1][key[1]-1] for key in s if key[2] == t) <= self.store["K"][t-1])
 
-        # lager periode 0
+        # Initial warehouse stock
         for p in prodRange:
             logging.debug("%s == %s", l[p, 0].varName, self.store["l"][p-1])
             model.addConstr(l[p, 0] == self.store["l"][p-1])
-        # lager ab periode 1
+        # Update warehouse stock inbetween periods
         for t in range(1, tHor+1):
             for p in prodRange:
                 logging.debug("{} = {} + {} - {}".format(l[p, t].varName, l[p, t-1].varName, x[p, t].varName, self.store["d"][p-1][t-1]))
@@ -210,6 +178,7 @@ class LotSolver(object):
         # solve it
         model.optimize()
 
+        # Debugging printouts
         for y in model.getVars():
             if y.x >= 0.001:
                 logging.debug("%s = %s cost %d", y.varName, y.x, y.x*y.obj)
