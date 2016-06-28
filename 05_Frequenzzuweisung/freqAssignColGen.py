31087f59cdf363928d22bd4c5f18d361455ae9a4
Dominik Chmiel <dominik.chmiel@rwth-aachen.de>
2016-06-28 17:42:19 +0200

    Initial commit for 05

---
+++ 05_Frequenzzuweisung/freqAssignColGen.py
@@ -0,0 +1,18 @@
+from gurobipy import *
+
+def solve(positions, radius):
+
+  print("%d Antennen, Radius = %g" % (len(positions), radius))
+
+  # Hier fehlt Code.
+
+  rmp = Model("Frequency assignment RMP")
+
+  # Hier fehlt Code.
+
+  pp = Model("Frequency assignment PP")
+
+  # Hier fehlt Code.
+
+  # Essenzielle Bedingung: Das RMP- und PP-Modellobjekt muessen zurueckgegeben werden!
+  return (rmp, pp)
